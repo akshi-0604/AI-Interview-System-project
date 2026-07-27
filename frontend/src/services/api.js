416e80a978f7bb0b93ed5be1cd86cf00847fffe8
akshi-0604 <akshitha@shnoor.com>
2026-07-27 11:43:11 +0530

    Fixed production API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,37 +1,20 @@
 import axios from "axios";
 
-
 const API = axios.create({
-
-    baseURL: "https://ai-interview-system-project.onrender.com",
-
+  baseURL: "https://ai-interview-system-project.onrender.com/api",
 });
 
-
-// Automatically attach JWT token
 API.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
 
-    (config) => {
-
-        const token = localStorage.getItem("token");
-
-        if (token) {
-
-            config.headers.Authorization = `Bearer ${token}`;
-
-        }
-
-        return config;
-
-    },
-
-    (error) => {
-
-        return Promise.reject(error);
-
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
     }
 
+    return config;
+  },
+  (error) => Promise.reject(error)
 );
-
 
 export default API;
